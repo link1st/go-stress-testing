d476c45a2c6d12c696e04bca161757089c8bfda3
link1st <562117637@qq.com>
2023-10-13 10:32:44 +0800

    add linux arm64

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ go build -ldflags "-s -w" -o go-stress-testing-mac main.go
 
 # 使用交叉编译 linux和windows版本可以执行的文件
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o go-stress-testing-linux main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w" -o go-stress-testing-linux-arm64 main.go
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o go-stress-testing-win.exe main.go
